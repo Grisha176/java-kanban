1263e4cbc2317b75a77687ec86674329e7e36566
Grisha <gnavi00@mail.ru>
2025-01-20 15:47:45 +0300

    исправления для тестов на гитхаб

--- src/taskmanager/Epic.java
+++ src/taskmanager/Epic.java
@@ -21,7 +21,7 @@ public class Epic extends Task {
 
     @Override
     public LocalDateTime getEndTime() {
-        return manager.getSubTaskSortByTime().get(manager.getSubTaskSortByTime().size()-1);
+        return manager.getSubTaskSortByTime().get(manager.getSubTaskSortByTime().size() - 1);
     }
 
     public int getId() {
